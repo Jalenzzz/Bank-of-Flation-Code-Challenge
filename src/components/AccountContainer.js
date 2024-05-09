19545ddc66c5ecb04d59cbe431c3f0c2faf60173
Jalen <mnenejalen72@gmail.com>
2024-05-09 13:41:46 +0300

    Fixed the table to see all transactions

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
+  const [transactions, setTransactions] = useState([]);
+  const getTransactions = () => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch("http://localhost:8001/transactions", requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        setTransactions(result);
+      })
+      .catch((error) => console.error(error));
+  };
+
+  useEffect(() => {
+    getTransactions();
+  }, []);
+
   return (
     <div>
       <Search />
       <AddTransactionForm />
-      <TransactionsList />
+      <TransactionsList transactions={transactions} />
     </div>
   );
 }
